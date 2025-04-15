515d3c09958faee01f52b44ada0772b62b3601cf
Neranjana Prasad <neranjanaprsd@gmail.com>
2025-04-15 16:53:32 +0530

    chore: add vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Footer from "@/components/footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
       <body className={cn(inter.className, "dark")}>
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
