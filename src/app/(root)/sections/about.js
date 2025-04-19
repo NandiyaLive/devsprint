73498e4ee679c2da864670271b22c523f6bdb42e
Neranjana Prasad <neranjanaprsd@gmail.com>
2025-04-19 09:02:35 +0530

    feat: add media partners

--- src/app/(root)/sections/about.js
+++ src/app/(root)/sections/about.js
@@ -2,14 +2,16 @@ import { speakersList } from "@/data/speakers";
 import Image from "next/image";
 import gceLogo from "@public/logos/gce.png";
 import wso2Logo from "@public/logos/wso2.png";
+import fotsLogo from "@public/logos/fots.png";
+import vvLogo from "@public/logos/vv.png";
 
 const About = () => {
   return (
     <section className="mt-20 pt-20" id="speakers">
-      <div className="container flex flex-col gap-4">
+      <div className="container">
         <h1 className="text-5xl font-semibold">About</h1>
 
-        <p className="text-lg">
+        <p className="mt-4 mb-8 text-lg">
           DevSprint is a full-day interactive event designed to empower
           undergraduates with hands-on experience across AI, Mobile and Cloud,
           which is organized by IEEE WIE Student Branch of University of
@@ -21,42 +23,48 @@ const About = () => {
 
         <hr />
 
-        <div className="grid max-w-xl grid-cols-2 gap-8">
+        <div className="mt-8 grid grid-cols-2 gap-8 md:grid-cols-4">
           <div>
-            <h2 className="text-6xl">3</h2>
-            <p className="mt-2 text-xs">Trending Tracks</p>
-          </div>
-
-          <div>
-            <h2 className="text-6xl">5</h2>
-            <p className="mt-2 text-xs">Github Campus Experts</p>
-          </div>
-        </div>
-
-        <hr />
-
-        <div className="grid max-w-xl grid-cols-2 gap-8">
-          <div>
-            <p>Title Sponsor</p>
+            <p className="text-sm">Knowledge Partner</p>
 
             <Image
               src={gceLogo}
               alt="GCE Logo"
-              className="mt-4 h-16 w-auto"
+              className="mt-4 h-12 w-auto"
               width={100}
               height={100}
             />
           </div>
 
           <div>
-            <p>Bronze Sponsor</p>
+            <p className="text-sm">Bronze Sponsor</p>
             <Image
               src={wso2Logo}
               alt="WSO2 Logo"
-              className="mt-4 h-16 w-auto"
+              className="mt-4 h-12 w-auto"
               width={100}
               height={100}
             />
+          </div>
+
+          <div className="col-span-2">
+            <p className="text-sm">Media Partners</p>
+            <div className="grid grid-cols-2 gap-8">
+              <Image
+                src={vvLogo}
+                alt="Vanni Vogue Logo"
+                className="mt-4 h-12 w-auto"
+                width={100}
+                height={100}
+              />
+              <Image
+                src={fotsLogo}
+                alt="FOST Media Logo"
+                className="mt-4 h-12 w-auto"
+                width={100}
+                height={100}
+              />
+            </div>
           </div>
         </div>
       </div>
